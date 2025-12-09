42a5de3fd97322f02b03bc05b06cb79883350085
rohitsg03 <rohit.gelaudie@hva.nl>
2025-12-10 00:13:38 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,26 @@
-/******************************/
 /* menu openen de MENU button */
-/******************************/
-
 // stap 1: zoek de menu-button op en sla die op in een variabele
 var openButton;
 openButton = document.querySelector("header section > button");
 
-
 // stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
 openButton.onclick = openMenu;
 
-
 // stap 3: voeg in de functie een class toe aan de nav
-function openMenu(){
+function openMenu() {
   var deNav;
   deNav = document.querySelector("nav");
   deNav.classList.add("toonMenu");
-};
+}
 
 
-
-
-/************************************/
 /* menu sluiten met de sluit button */
-/************************************/
-
 // stap 1 - zoek sluiten button op
 var sluitButton;
 sluitButton = document.querySelector("nav button");
 
 // stap 2 - laat die button luisteren naar kliks
 sluitButton.onclick = sluitMenu;
-
 
 // stap 3 - in de functie verwijder de class van de nav
 function sluitMenu() {
@@ -43,27 +32,13 @@ function sluitMenu() {
 
 // Sluiten van Spotify atricle
 // Bron: de student-assistenten
-let deleteSpotify = document.querySelector("main > article:nth-of-type(5) button");
+let deleteSpotify = document.querySelector(
+  "main > article:nth-of-type(5) button"
+);
 
 deleteSpotify.onclick = sluitSpotify;
 
-function sluitSpotify(){
+function sluitSpotify() {
   let spotifyArticle = document.querySelector("main > article:nth-of-type(5)");
   spotifyArticle.classList.add("hidden");
 }
-
-
-// // Sterknop oranje maken
-// let sterOranje = document.querySelector("section article button");
-
-// sterOranje.onclick = oranjeMaken;
-
-// function oranjeMaken(){
-//   console.log("hallo")  // let sterOranje = document.querySelector("section article button svg");
-//   // sterOranje.classList.add("")
-// }
-
-
-
-
-
